79f16d0e12db28a17b96564700965c6c035a8d68
Md Rakibul Islam <bdsky25@gmail.com>
2024-02-06 00:17:09 +0600

    Implement VerifyAdmin Middleware and secure Users API

--- index.js
+++ index.js
@@ -55,6 +55,17 @@ async function run() {
             res.send({ token });
         });
 
+        // VerifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        };
+
         // Admin Related APIs
         app.get('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
@@ -70,7 +81,7 @@ async function run() {
         });
 
         // User Related APIs
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
